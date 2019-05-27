5326353e89108be399a05527ff2cd9d107cff057
solomon <shaminsolomonit@gmail.com>
2019-05-27 09:19:04 +0530

    "algo expert question solved in (n logn) time complexity"

---
+++ src/test/java/com/solo/algoexpert/easy/TwonumberSumNospaceTest.java
@@ -0,0 +1,24 @@
+package com.solo.algoexpert.easy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwonumberSumNospaceTest {
+    private TwonumberSumNospace twonumberSumNospace;
+
+    @BeforeEach
+    void setUp() {
+        twonumberSumNospace = new TwonumberSumNospace ();
+    }
+
+    @Test
+    void baseCase() {
+        int[] input = {3,5,-4,8,11,1,-1,6};
+        int[] result = twonumberSumNospace.computeSum(input,10);
+        int[] expectedResult = {-1,11};
+        Assertions.assertArrayEquals (result, expectedResult);
+    }
+}
