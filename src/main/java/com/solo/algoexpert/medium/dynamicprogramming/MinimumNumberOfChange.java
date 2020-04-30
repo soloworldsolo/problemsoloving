69955434051a86a22450a6012c6702b86d4dc78b
solomon <shaminsolomonit@gmail.com>
2020-05-01 01:37:20 +0530

    "optimisation of MinimumNumberOfChange "

--- src/main/java/com/solo/algoexpert/medium/dynamicprogramming/MinimumNumberOfChange.java
+++ src/main/java/com/solo/algoexpert/medium/dynamicprogramming/MinimumNumberOfChange.java
@@ -1,24 +1,31 @@
 package com.solo.algoexpert.medium.dynamicprogramming;
 
+import static java.lang.Integer.MAX_VALUE;
+
 import java.util.Arrays;
 
+/**
+ * Given a value V, if we want to make change for V cents, and we have infinite supply of each of C
+ * = { C1, C2, .. , Cm} valued coins, what is the minimum number of coins to make the change?
+ *
+ * Input: coins[] = {25, 10, 5}, V = 30
+ *
+ * output = 2
+ */
 public class MinimumNumberOfChange {
 
   public int minNumberOfCoinsForChange(int n, int[] denoms) {
     int[] result = new int[n + 1];
     Arrays.fill(result, Integer.MAX_VALUE);
     result[0] = 0;
-    int toCompare = Integer.MAX_VALUE;
-    for (int coin : denoms) {
-      for (int i = 1; i < n + 1; i++) {
-        if (coin <= n && i >= coin) {
-          if (result[i - coin] != Integer.MAX_VALUE) {
-            toCompare = 1 + result[i - coin];
-          }
-          result[i] = Math.min(result[i], toCompare);
+    for (int i = 0; i < denoms.length; i++) {
+      for (int j = 1; j < result.length; j++) {
+        if (denoms[i] <= n && j >= denoms[i] && result[j - denoms[i]] != MAX_VALUE) {
+          result[j] = Math.min(result[j], 1 + result[j - denoms[i]]);
         }
       }
     }
-    return result[n];
+
+    return result[n] == MAX_VALUE ? -1 : result[n];
   }
 }
